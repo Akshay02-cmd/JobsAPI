929d5a89e458279ba4f0596d2fba38ed262fb6bf
Akshay02-cmd <akshaydpatil1646@gmail.com>
2025-11-19 22:04:46 +0530

    enhance error handling middleware with improved error messages and status codes

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,34 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.message || 'Something went wrong, try again later' })
-}
+  let CustomError = {
+    statusCode: err.StatusCodes || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, try again later",
+  };
 
-module.exports = errorHandlerMiddleware
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+if (err.name === "ValidationError") {
+    CustomError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+    CustomError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if(err.name === "CastError") {
+    CustomError.msg = `No item found with id : ${err.value}`;
+    CustomError.statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  if (err.code && err.code === 11000) {
+    CustomError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
+    CustomError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  // return res
+  //   .status(StatusCodes.INTERNAL_SERVER_ERROR)
+  //   .json("Something went wrong, try again later");
+  return res.status(CustomError.statusCode).json({ msg: CustomError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
