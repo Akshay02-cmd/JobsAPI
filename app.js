7c99426d01619ef4b446226998a5a4c33f8d48de
Akshay02-cmd <akshaydpatil1646@gmail.com>
2025-11-19 22:15:30 +0530

    add security middleware and update dependencies for enhanced protection

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 require("dotenv").config();
+
+//extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -15,6 +22,15 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, //limit each IP to 100 requests per windowMs
+  })
+);
 
 // routes
 app.use("/api/v1/auth", authrouter);
