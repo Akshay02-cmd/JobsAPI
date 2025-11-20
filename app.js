bd6c02f7f03dd11512cc853801782e467730bb66
Akshay02-cmd <akshaydpatil1646@gmail.com>
2025-11-20 12:49:12 +0530

    reorder middleware setup for improved security configuration

--- app.js
+++ app.js
@@ -20,17 +20,18 @@ const jobrouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(express.json());
-// extra packages
-app.use(helmet());
-app.use(cors());
-app.use(xss());
+// extra security packages
+app.set("trust proxy", 1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, //15 minutes
     max: 100, //limit each IP to 100 requests per windowMs
   })
 );
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authrouter);
