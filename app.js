07ae9a4c950a88295fe0c34745cb9baa6c0f85d7
Akshay02-cmd <akshaydpatil1646@gmail.com>
2025-11-20 18:14:40 +0530

    feat: add Swagger documentation for API endpoints and update swagger-ui-express dependency

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const helmet = require("helmet");
 const cors = require("cors");
 const rateLimiter = require("express-rate-limit");
 
+//swagger setup
+const swaggerUI =  require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -32,7 +37,11 @@ app.use(
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+// app.use(xss()); --- IGNORE ---
+// app.use(mongoSanitize()); --- IGNORE ---
 
+//swagger route
+app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 app.use("/api/v1/auth", authrouter);
 app.use("/api/v1/jobs", authenticateUser, jobrouter);
