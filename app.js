04e03ffa86fbb5511c24cdbc4baa2cf873b8cff6
Akshay02-cmd <akshaydpatil1646@gmail.com>
2025-11-20 13:10:38 +0530

    removing xss clean because it modifying the req.query; which cuases the erroro {msg:"Cannot set property query of"}

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 //extra security packages
 const helmet = require("helmet");
 const cors = require("cors");
-const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
 const express = require("express");
@@ -26,12 +25,13 @@ app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, //15 minutes
     max: 100, //limit each IP to 100 requests per windowMs
+    standardHeaders: true,
+    legacyHeaders: false,
   })
 );
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authrouter);
